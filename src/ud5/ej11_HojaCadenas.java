3dff34431444abec599eeda7fa2f9a89dd5c83b3
NoeG47 <hellobrawlstars2.0@gmail.com>
2023-12-15 09:13:27 +0100

    ejercicio 11 incompleto

--- src/ud5/ej11_HojaCadenas.java
+++ src/ud5/ej11_HojaCadenas.java
@@ -1,10 +1,39 @@
 package ud5;
 
+import java.util.Scanner;
+
+/*
+ *  Realizar una función a la que se le pasa por parámetro una cadena
+con un email, y dicha función devuelve si el mail está bien formado.
+Un email está bien formado cuando:
+1.1.- Contiene una y solo una arroba.
+1.2.- No puede contener dos puntos seguidos '.' después de la arroba.
+1.3.- No puede contener ' '
+*/
 public class ej11_HojaCadenas {
+	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int contador = 0;
+		int i = 0;
+		boolean contieneArroba = false;
+		System.out.println("Introduce un correo: ");
+		String cad = in.nextLine();
+		// comprobar letra a letra si hay alguna arroba
+		for ( i = 0; i < cad.length(); i++) {
+			if (cad.charAt(i) == '@') {
+				contieneArroba = true;
+				contador++;
+				System.out.println("el @ se encuentra en la posicion " +  (i+1));
+			}
+		}
+		if (contieneArroba == true && contador == 1) {
+            System.out.println("Contiene un arroba");
+         
+        } else if (contieneArroba == true && contador > 1) {
+            System.out.println("No puede haber más de dos @");
+        } else {
+        	System.out.println("No hay @");
+        }
 	}
-
 }
