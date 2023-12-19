d040287aafc92520a0f5296c24f09eddf6c7b2db
NoeG47 <hellobrawlstars2.0@gmail.com>
2023-12-19 12:34:32 +0100

    triples

---
+++ src/ProyectoSegundaEva/Proyecto_wordle.java
@@ -0,0 +1,98 @@
+package ProyectoSegundaEva;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Proyecto_wordle {
+	static Scanner in = new Scanner(System.in);
+	static String palabraSecreta = " ";
+	static int numIntentosConsumidos = 6;
+	static int numLetrasAdivinadas = 0;
+	static int i = 0;
+
+	public static void main(String[] args) {
+		String[] palabras = { "robar", "comer", "lapiz", "camion", "juego", "letra", "jorge", "gafas", "manta", "queso",
+				"ranas", "canoa", "avion", "lento", "novio", "cable", "raton", "color", "reina", "cocos" };
+		Random ale = new Random();
+		int numAle = ale.nextInt(21);
+		palabraSecreta = palabras[numAle];
+		char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+
+		System.out.println(palabraSecreta);
+		// do {
+		System.out.println("Introduce una palabra de 5 letras");
+		String palabraIntroducida = in.nextLine().toLowerCase(); // convertimos a minusculas
+		if (!contiene5letras(palabraIntroducida)) {
+			System.out.println("Debe ser una palabra de 5 letras");
+		}
+		if (!contieneSoloLetras(palabraIntroducida)) {
+			System.out.println("Solo se admiten letras");
+
+		}
+		if (contieneVocales(palabraIntroducida, vocales) == true) {
+			System.out.println("contiene una vocal pero se necesitan mas de 2 y maximo de 3");
+		} else {
+			System.out.println("ta mal");
+		}
+
+		// } while ();
+
+	}
+	/*
+	 * public static boolean haGanadoJugador1(String cad) {
+	 * 
+	 * } public static boolean haTerminadoJuego(String cad, intentos) {
+	 * 
+	 * } public static String compruebaLetrasAcertadas(String cad) {
+	 * 
+	 * } public static String generaPalabra() {
+	 * 
+	 * }
+	 */
+
+	// COMPROBACIONES
+	public static boolean contiene5letras(String cad) {
+		if (cad.length() > 0 && cad.length() < 5) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean contieneSoloLetras(String cad) {
+		for (i = 0; i < cad.length(); i++) {
+			char caracter = cad.charAt(i);
+			// Verifica si el carácter no es una letra
+			if (!Character.isLetter(caracter)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean contieneVocales(String cad, char[] vocales) {
+		for (i = 0; i < cad.length(); i++) {
+			for (int j = 0; j < vocales.length; j++) {
+				if (cad.charAt(i) == vocales[i]) {
+					return true;
+				}
+			}
+			
+		}
+		return false;
+	}
+
+	/*public static int contarVocales(String cad, char[] vocales) {
+		int cont = 0;
+		for (i = 0; i < cad.length(); i++) {
+			char letraActual = cad.charAt(i);
+			for (int j = 0; j < vocales.length; j++) {
+				if (letraActual == vocales[j]) {
+					cont++;
+				}
+			}
+
+		}
+		return cont;
+	}*/
+
+}
---
+++ src/ud5/Ej11_hojaCadenasSolPaul.java
@@ -0,0 +1,52 @@
+package ud5;
+
+import java.util.Scanner;
+
+public class Ej11_hojaCadenasSolPaul {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		System.out.println("Introduce tu correo");
+		String email = in.nextLine();
+		
+		if(emailEsCorrecto(email) == true) {
+			System.out.println("El correo esta bien formado");
+		}
+		else {
+			System.out.println("El correo no esta bien formado");
+		}
+	}
+	public static boolean emailEsCorrecto(String email) {
+		if(!contieneUnaArriba(email))
+			return false;
+		if(contieneAlgunEspacio(email))
+			return false;
+		return true;
+	}
+	public static boolean contieneUnaArriba(String email) {
+		int posPrimeraArroba = email.indexOf('@');
+		int posUltimaArroba = email.lastIndexOf('@');
+		if (posPrimeraArroba == -1)
+			return false;
+		//solo hay un @
+		if (posPrimeraArroba == posUltimaArroba)
+			return true;
+		// hay más de 1 arroba
+		return false;
+	}
+	public static boolean contieneAlgunEspacio(String email) {
+		//forma 1
+		/*
+		for(int i = 0; i < email.length(); i++) {
+			if (email.charAt(i)=' ')
+				return false;
+		}
+		return true;*/
+		//forma 2
+		if (email.contains(" "))
+			return true;
+		//si no lo contiene
+		return false;
+	}
+}
