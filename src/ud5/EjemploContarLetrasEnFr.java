7e49cbfa19a664894af599f0972d72900f7b3a04
NoeG47 <hellobrawlstars2.0@gmail.com>
2024-01-09 12:33:59 +0100

    ContarPalabrasAscii+casteo

---
+++ src/ud5/EjemploContarLetrasEnFr.java
@@ -0,0 +1,33 @@
+package ud5;
+
+//Hacer un programa que lea una frase e indique cuántas veces aparece cada letra en dicha frase
+import java.util.Scanner;
+
+public class EjemploContarLetrasEnFr {
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int numLetrasAlfabeto = 'z' - 'a' + 1;
+		int[] contLetras = new int[numLetrasAlfabeto];
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Introduce una frase");
+		String frase = in.nextLine();
+		frase = frase.toLowerCase();
+		// recorremos la frase letra a letra
+		for (int i = 0; i < frase.length(); i++) {
+			// transformo la letra en un índice
+			// 'a' - 0
+			// 'b' - 1 etc
+			int pos = frase.charAt(i) - 'a';
+			if ((pos >= 0) && (pos <= numLetrasAlfabeto))
+				contLetras[pos]++;
+		}
+		for (int i = 0; i < numLetrasAlfabeto; i++) {
+			if (contLetras[i] > 0)
+				System.out.println((char) (i + 'a') + " " + contLetras[i]);
+		}
+
+	}
+
+}
