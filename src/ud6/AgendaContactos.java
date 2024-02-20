aa162e4df66e2727a1f7895d8b5926ed5d9fc11c
NoeG47 <hellobrawlstars2.0@gmail.com>
2024-02-20 09:50:38 +0100

    EjercicioAgenda_Contactos_Acabado: acabar main de usoAgenda

--- src/ud6/AgendaContactos.java
+++ src/ud6/AgendaContactos.java
@@ -4,71 +4,116 @@ public class AgendaContactos {
 	private final int TAM = 10;
 	private int cont;
 	private Contacto[] agenda;
-	
-	//constructor
+
+	// constructor
 	public AgendaContactos() {
 		this.agenda = new Contacto[TAM];
 
 	}
-	//para devolver la agenda
+
+	// para devolver la agenda
 	public Contacto[] getAgenda() {
 		return agenda;
 	}
+
 	public void setAgenda(Contacto[] agenda) {
 		this.agenda = agenda;
 	}
+
 	public void aniadirContacto(Contacto contacto) {
 		if (cont < TAM) {
 			for (int i = 0; i < TAM; i++) {
 				if (agenda[i] == null) {
-					agenda[i] = contacto;
-					System.out.println(contacto.toString());
-					System.out.println("Se ha añadido correctamente");
-					i = TAM;
-					cont++;
+					if (!existeContacto(contacto)) {
+						agenda[i] = contacto;
+						System.out.println(contacto.toString());
+						System.out.println("Se ha añadido correctamente");
+						i = TAM;
+						cont++;
+					}
 				}
 
 			}
-		}
-		else {
-			System.out.println("no se ha añadido");
+		} else {
+			System.out.println("Agenda llena, no se ha añadido");
 		}
 	}
+	// esto funciona si en lugar de un string buscamos por el objeto contacto
+	/*
+	 * private int encontrarPosicionContacto(Contacto contacto) { for (int i = 0; i
+	 * < TAM; i++) { String nombre = contacto.getNombre(); if (agenda[i] != null &&
+	 * agenda[i].getNombre().equals(nombre)) { return i; // R } } return -1; //
+	 * Retorna -1 si el contacto no se encuentra }
+	 */
+
+	/*
+	 * public void buscarContacto(Contacto contacto) {
+	 * 
+	 * if (existeContacto(contacto)) { int pos =
+	 * encontrarPosicionContacto(contacto);
+	 * System.out.println(agenda[pos].toString()); } else {
+	 * System.out.println("El contacto no existe"); }
+	 * 
+	 * }
+	 */
+	public void buscarContacto(String contacto) {
+		boolean contactoEncontrado = false;
+		for (int i = 0; i < TAM; i++) {
+			if (agenda[i] != null && agenda[i].getNombre().equals(contacto)) {
+				System.out.println(agenda[i].getTelefono());
+				contactoEncontrado = true;
+				i = TAM;
+			}
+		}
+		if (!contactoEncontrado)
+			System.out.println("Contacto no encontrado, no se puede mostrar su número de teléfono");
+	}
+
 	public boolean existeContacto(Contacto contacto) {
+		for (int i = 0; i < TAM; i++) {
+			String nombre = contacto.getNombre();
+			if (agenda[i] != null && agenda[i].getNombre().equals(nombre)) {
+				return true;
+			}
+		}
 		return false;
-		
+
 	}
+
 	public boolean eliminarContacto(Contacto contacto) {
 		for (int i = 0; i < TAM; i++) {
 			String nombre = contacto.getNombre();
 			if (agenda[i] != null && agenda[i].getNombre().equals(nombre)) {
 				agenda[i] = null;
-				cont = cont -1;
+				cont = cont - 1;
 				return true;
 			}
 		}
 		return false;
 	}
+
 	public int huecosLibre() {
-			int huecos = TAM - cont;
-			return huecos;
-		
-		
+		int huecos = TAM - cont;
+		return huecos;
+
 	}
+
 	public boolean agendaLlena() {
 		if (cont == TAM) {
 			return true;
 		}
 		return false;
 	}
+
 	public void listarContactos() {
 		System.out.println(toString());
 	}
+
 	public String toString() {
 		String cadContactos = "";
 		for (int i = 0; i < TAM; i++) {
 			if (agenda[i] != null) {
-				cadContactos += agenda[i].toString()+"\n";
+				cadContactos += agenda[i].toString() + "\n";
 			}
 
 		}
--- src/ud6/UsoAgenda.java
+++ src/ud6/UsoAgenda.java
@@ -13,10 +13,26 @@ public class UsoAgenda {
 		agenda.aniadirContacto(user2);
 		agenda.aniadirContacto(user3);
 		agenda.eliminarContacto(user2);
+		agenda.eliminarContacto(user3);
+		System.out.println("------------");
 		agenda.listarContactos();
-		System.out.println("te quedan " + agenda.huecosLibre() + "espacios");
+		System.out.println("te quedan " + agenda.huecosLibre() + " espacios");
+		agenda.aniadirContacto(user2);
+		agenda.aniadirContacto(user2);
 		
-		
+		if(agenda.existeContacto(user2)) {
+			System.out.println("No se ha añadido, contacto duplicado");
+		}
+		agenda.aniadirContacto(user2);
+		if(agenda.agendaLlena()) {
+			System.out.println("agenda llena");
+		}
+		else {
+			System.out.println("agenda no llena");
+		}
+
+		System.out.println("te quedan " + agenda.huecosLibre() + " espacios");
+		agenda.buscarContacto("carlo8");
 	}
 
 }
