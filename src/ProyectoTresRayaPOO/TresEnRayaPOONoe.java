7f0db46e15f8c2e91aadb5e52775e68080929f7a
NoeG47 <hellobrawlstars2.0@gmail.com>
2024-03-20 07:27:51 +0100

    tresEnRaya

--- src/ProyectoTresRayaPOO/TresEnRayaPOONoe.java
+++ src/ProyectoTresRayaPOO/TresEnRayaPOONoe.java
@@ -4,307 +4,285 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TresEnRayaPOONoe {
-    private static int[][] tablero;
-    private static int jugadorActual;
-    private static Scanner entrada;
-    private static Random random;
-    private static int[] estadisticas;
+	private static int[][] tablero;
+	private static int jugadorActual;
+	private static Scanner entrada;
+	private static Random random;
+	private static int[] estadisticas;
 
-    public TresEnRayaPOONoe() {
-        random = new Random();
-        entrada = new Scanner(System.in);
-        estadisticas = new int[3];
-    }
+	public TresEnRayaPOONoe() {
+		random = new Random();
+		entrada = new Scanner(System.in);
+		estadisticas = new int[3];
+		tablero = new int[3][3];
+	}
 
-    public void iniciarJuego() {
-        tablero = new int[3][3];
-        seleccionarModalidad();
-    }
+	private void reiniciarTablero() {
+		tablero = new int[3][3];
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				tablero[i][j] = 0;
+			}
+		}
+	}
 
-    private void reiniciarTablero() {
-        tablero = new int[3][3];
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                tablero[i][j] = 0;
-            }
-        }
-    }
+	public void jugarPartida() {
+		reiniciarTablero();
+		boolean continuarJuego = true;
+		determinarPrimerJugador();
+		while (continuarJuego) {
+			System.out.println("Turno del jugador " + jugadorActual);
+			dibujarTablero();
+			System.out.println("Ingrese la posición (1-9): ");
+			int posicion = entrada.nextInt();
+			if (movimientoValido(posicion)) {
+				if (jugadorActual == 1) {
+					mueveJugador1(posicion);
+				} else {
+					mueveJugador2(posicion);
+				}
+			} else {
+				System.out.println("Posición no válida, intente nuevamente.");
+				continue;
+			}
+			dibujarTablero();
 
-    private void seleccionarModalidad() {
-        System.out.println("Bienvenido al tres en raya de Noe");
-        System.out.println("Seleccione la modalidad de juego:");
-        System.out.println("1. 2 Jugadores");
-        System.out.println("2. Jugador vs Máquina");
-        System.out.println("5. Salir del programa");
-        int opcion = entrada.nextInt();
+			if (ganaJugador1()) {
+				System.out.println("¡Jugador 1 ha ganado!");
+				estadisticas[0]++;
+				continuarJuego = false;
 
-        switch (opcion) {
-            case 1:
-                jugarDosJugadores();
-                break;
-            case 2:
-                jugarConMaquina();
-                break;
-            case 5:
-                System.out.println("Hasta luego");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Opción no válida. Por favor, seleccione nuevamente.");
-                seleccionarModalidad();
-                break;
-        }
-    }
+			} else if (ganaJugador2()) {
+				System.out.println("¡Jugador 2 ha ganado!");
+				estadisticas[1]++;
+				continuarJuego = false;
+			} else if (quedanMovimientos()) {
+				if (jugadorActual == 1) {
+					jugadorActual = 2;
+				} else {
+					jugadorActual = 1;
+				}
+			} else {
+				System.out.println("¡Empate!");
+				estadisticas[2]++;
+				continuarJuego = false;
+			}
+		}
 
-    private void jugarDosJugadores() {
-        determinarPrimerJugador();
-        jugarPartida();
-    }
+	}
 
-    private void jugarConMaquina() {
-        determinarPrimerJugador();
-        jugarPartidaConMaquina();
-    }
+	public void jugarPartidaConMaquina() {
+		reiniciarTablero();
+		boolean continuarJuego = true;
+		boolean posicionEsValida = true;
+		determinarPrimerJugador();
+		while (continuarJuego) {
+			int posicion;
+			if (jugadorActual == 1) {
+				dibujarTablero();
+				System.out.println("Turno del jugador " + jugadorActual);
+				do {
+					System.out.println("Ingrese la posición (1-9): ");
+					posicion = entrada.nextInt();
+					if (movimientoValido(posicion)) {
+						mueveJugador1(posicion);
+						posicionEsValida = true;
+					} else {
+						posicionEsValida = false;
+						System.out.println("Posición inválida, intente nuevamente.");
 
-    public void jugarPartida() {
-        reiniciarTablero();
-        boolean continuarJuego = true;
+					}
+				} while (!posicionEsValida);
+			} else {
+				System.out.println("Turno de la máquina");
+				posicion = generarMovimientoMaquina();
+				mueveJugador2(posicion);
+				System.out.println("La máquina ha movido en la posición " + posicion);
+			}
 
-        while (continuarJuego) {
-            System.out.println("Turno del jugador " + jugadorActual);
-            dibujarTablero();
-            System.out.println("Ingrese la posición (1-9): ");
-            int posicion = entrada.nextInt();
-            if (movimientoValido(posicion)) {
-                if (jugadorActual == 1) {
-                    mueveJugador1(posicion);
-                } else {
-                    mueveJugador2(posicion);
-                }
-            } else {
-                System.out.println("Posición inválida, intente nuevamente.");
-                continue;
-            }
-            dibujarTablero();
+			if (ganaJugador1()) {
+				System.out.println("¡Jugador 1 ha ganado!");
+				estadisticas[0]++;
+				continuarJuego = false;
 
-            if (ganaJugador1()) {
-                System.out.println("¡Jugador 1 ha ganado!");
-                estadisticas[0]++;
-                continuarJuego = false;
+			} else if (ganaJugador2()) {
+				System.out.println("¡La máquina ha ganado!");
+				estadisticas[1]++;
+				continuarJuego = false;
+			} else if (quedanMovimientos()) {
+				if (jugadorActual == 1) {
+					jugadorActual = 2;
+				} else {
+					jugadorActual = 1;
+				}
+			} else {
+				System.out.println("¡Empate!");
+				estadisticas[2]++;
+				continuarJuego = false;
+			}
+		}
+	}
 
-            } else if (ganaJugador2()) {
-                System.out.println("¡Jugador 2 ha ganado!");
-                estadisticas[1]++;
-                continuarJuego = false;
-            } else if (quedanMovimientos()) {
-                if (jugadorActual == 1) {
-                    jugadorActual = 2;
-                } else {
-                    jugadorActual = 1;
-                }
-            } else {
-                System.out.println("¡Empate!");
-                estadisticas[2]++;
-                continuarJuego = false;
-            }
-        }
-        entrada.nextLine(); // limpiamos el buffer
-        seleccionarModalidadJvsJ();
+	public void jugarMaquinaConMaquina() {
+		reiniciarTablero();
+		boolean continuarJuego = true;
+		determinarPrimerJugador();
 
-    }
+		while (continuarJuego) {
+			int posicion;
 
-    public void jugarPartidaConMaquina() {
-        reiniciarTablero();
-        boolean continuarJuego = true;
+			if (jugadorActual == 1) {
 
-        while (continuarJuego) {
-            System.out.println("Turno del jugador " + jugadorActual);
-            dibujarTablero();
-            
-            if (jugadorActual == 1) {
-                System.out.println("Ingrese la posición (1-9): ");
-                int posicion = entrada.nextInt();
-                if (movimientoValido(posicion)) {
-                    mueveJugador1(posicion);
-                } else {
-                    System.out.println("Posición inválida, intente nuevamente.");
-                    continue;
-                }
-            } else {
-                // Movimiento de la máquina
-                int posicion = random.nextInt(9) + 1;
-                while (!movimientoValido(posicion)) {
-                    posicion = random.nextInt(9) + 1;
-                }
-                mueveJugador2(posicion);
-                System.out.println("La máquina ha movido en la posición " + posicion);
-            }
+				// Turno de la primera máquina
+				posicion = generarMovimientoMaquina();
+				mueveJugador1(posicion);
+				System.out.println("Máquina 1 ha movido en la posición " + posicion);
+				dibujarTablero();
 
-            dibujarTablero();
+			} else {
+				// Turno de la segunda máquina
 
-            if (ganaJugador1()) {
-                System.out.println("¡Jugador 1 ha ganado!");
-              
-                continuarJuego = false;
+				posicion = generarMovimientoMaquina();
+				mueveJugador2(posicion);
+				System.out.println("Máquina 2 ha movido en la posición " + posicion);
+				dibujarTablero();
+			}
 
-            } else if (ganaJugador2()) {
-                System.out.println("¡La máquina ha ganado!");
-                
-                continuarJuego = false;
-            } else if (quedanMovimientos()) {
-                if (jugadorActual == 1) {
-                    jugadorActual = 2;
-                } else {
-                    jugadorActual = 1;
-                }
-            } else {
-                System.out.println("¡Empate!");
-              
-                continuarJuego = false;
-            }
-        }
-        entrada.nextLine(); // limpiamos el buffer
-        seleccionarModalidad();
-    }
+			if (ganaJugador1()) {
+				System.out.println("¡Máquina 1 ha ganado!");
+				continuarJuego = false;
+			} else if (ganaJugador2()) {
+				System.out.println("¡Máquina 2 ha ganado!");
+				continuarJuego = false;
+			} else if (quedanMovimientos()) {
+				if (jugadorActual == 1) {
+					jugadorActual = 2;
+				} else {
+					jugadorActual = 1;
+				}
+			} else {
+				System.out.println("¡Empate!");
+				continuarJuego = false;
+			}
+		}
+	}
 
-    public void mostrarEstadisticas() {
-        boolean hayEstadisticas = false;
-        for (int i = 0; i < estadisticas.length; i++) {
-            if (estadisticas[i] != 0) {
-                hayEstadisticas = true;
-                i = estadisticas.length;
-            }
-        }
-        if (!hayEstadisticas) {
-            System.out.println("Debes jugar una partida para tener estadísticas");
-        } else {
-            System.out.println("Las victorias del Jugador 1 son: " + estadisticas[0] + " veces");
-            System.out.println("Las victorias del Jugador 2 son: " + estadisticas[1] + " veces");
-            System.out.println("El número de partidas empatadas son: " + estadisticas[2] + '\n');
-        }
-    }
+	private int generarMovimientoMaquina() {
+		int posicion = random.nextInt(9) + 1;
+		while (!movimientoValido(posicion)) {
+			posicion = random.nextInt(9) + 1;
+		}
+		return posicion;
+	}
 
-    public void reiniciarEstadisticas() {
-        if (estadisticas[0] == 0 && estadisticas[1] == 0 && estadisticas[2] == 0) {
-            System.out.println("Debes jugar al menos una vez para tener estadísticas antes de reiniciar.");
+	public void mostrarEstadisticas() {
+		boolean hayEstadisticas = false;
+		for (int i = 0; i < estadisticas.length; i++) {
+			if (estadisticas[i] != 0) {
+				hayEstadisticas = true;
+				i = estadisticas.length;
+			}
+		}
+		if (!hayEstadisticas) {
+			System.out.println("Debes jugar una partida para tener estadísticas");
+		} else {
+			System.out.println("Las victorias del Jugador 1 son: " + estadisticas[0] + " veces");
+			System.out.println("Las victorias del Jugador 2 son: " + estadisticas[1] + " veces");
+			System.out.println("El número de partidas empatadas son: " + estadisticas[2] + '\n');
+		}
+	}
 
-        } else {
-            estadisticas[0] = 0;
-            estadisticas[1] = 0;
-            estadisticas[2] = 0;
-        }
-    }
+	public void reiniciarEstadisticas() {
+		if (estadisticas[0] == 0 && estadisticas[1] == 0 && estadisticas[2] == 0) {
+			System.out.println("Debes jugar al menos una vez para tener estadísticas antes de reiniciar.");
 
-    public void dibujarTablero() {
-        System.out.println("-------------");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("| " + convertirNumeroACaracter(tablero[i][0]) + " | "
-                    + convertirNumeroACaracter(tablero[i][1]) + " | " + convertirNumeroACaracter(tablero[i][2]) + " |");
-            System.out.println("-------------");
-        }
-    }
+		} else {
+			estadisticas[0] = 0;
+			estadisticas[1] = 0;
+			estadisticas[2] = 0;
+		}
+	}
 
-    private void determinarPrimerJugador() {
-        int jugadorAleatorio = random.nextInt(2) + 1;
-        jugadorActual = jugadorAleatorio;
-        System.out.println("El jugador " + jugadorActual + " será el primero en jugar.");
-        if (jugadorActual == 1) {
-            System.out.println("El jugador 1 jugará con la ficha X.");
-        } else {
-            System.out.println("El jugador 2 jugará con la ficha O.");
-        }
-    }
+	public void dibujarTablero() {
+		System.out.println("-------------");
+		for (int i = 0; i < 3; i++) {
+			System.out.println("| " + convertirNumeroACaracter(tablero[i][0]) + " | "
+					+ convertirNumeroACaracter(tablero[i][1]) + " | " + convertirNumeroACaracter(tablero[i][2]) + " |");
+			System.out.println("-------------");
+		}
+	}
 
-    private boolean movimientoValido(int posicion) {
-        int fila = (posicion - 1) / 3;
-        int columna = (posicion - 1) % 3;
-        return (posicion >= 1 && posicion <= 9 && tablero[fila][columna] == 0);
-    }
+	private boolean movimientoValido(int posicion) {
+		int fila = (posicion - 1) / 3;
+		int columna = (posicion - 1) % 3;
+		return (posicion >= 1 && posicion <= 9 && tablero[fila][columna] == 0);
+	}
 
-    public void mueveJugador1(int pos) {
-        int fila = (pos - 1) / 3;
-        int columna = (pos - 1) % 3;
-        tablero[fila][columna] = 1;
-    }
+	public void mueveJugador1(int pos) {
+		int fila = (pos - 1) / 3;
+		int columna = (pos - 1) % 3;
+		tablero[fila][columna] = 1;
+	}
 
-    public void mueveJugador2(int pos) {
-        int fila = (pos - 1) / 3;
-        int columna = (pos - 1) % 3;
-        tablero[fila][columna] = 2;
-    }
+	public void mueveJugador2(int pos) {
+		int fila = (pos - 1) / 3;
+		int columna = (pos - 1) % 3;
+		tablero[fila][columna] = 2;
+	}
 
-    private char convertirNumeroACaracter(int num) {
-        if (num == 1) {
-            return 'X';
-        } else if (num == 2) {
-            return 'O';
-        } else {
-            return ' ';
-        }
-    }
+	private char convertirNumeroACaracter(int num) {
+		if (num == 1) {
+			return 'X';
+		} else if (num == 2) {
+			return 'O';
+		} else {
+			return ' ';
+		}
+	}
 
-    private boolean ganaJugador1() {
-        return hayTresEnLinea(1);
-    }
+	private boolean ganaJugador1() {
+		return hayTresEnLinea(1);
+	}
 
-    private boolean ganaJugador2() {
-        return hayTresEnLinea(2);
-    }
+	private boolean ganaJugador2() {
+		return hayTresEnLinea(2);
+	}
 
-    private boolean hayTresEnLinea(int jugador) {
-        for (int i = 0; i < 3; i++) {
-            if ((tablero[i][0] == jugador && tablero[i][1] == jugador && tablero[i][2] == jugador) || // Filas
-                    (tablero[0][i] == jugador && tablero[1][i] == jugador && tablero[2][i] == jugador)) { // Columnas
-                return true;
-            }
-        }
-        // Diagonales
-        if ((tablero[0][0] == jugador && tablero[1][1] == jugador && tablero[2][2] == jugador)
-                || (tablero[0][2] == jugador && tablero[1][1] == jugador && tablero[2][0] == jugador)) {
-            return true;
-        }
-        return false;
-    }
+	private boolean hayTresEnLinea(int jugador) {
+		for (int i = 0; i < 3; i++) {
+			if ((tablero[i][0] == jugador && tablero[i][1] == jugador && tablero[i][2] == jugador) || // Filas
+					(tablero[0][i] == jugador && tablero[1][i] == jugador && tablero[2][i] == jugador)) { // Columnas
+				return true;
+			}
+		}
+		// Diagonales
+		if ((tablero[0][0] == jugador && tablero[1][1] == jugador && tablero[2][2] == jugador)
+				|| (tablero[0][2] == jugador && tablero[1][1] == jugador && tablero[2][0] == jugador)) {
+			return true;
+		}
+		return false;
+	}
 
-    private boolean quedanMovimientos() {
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                if (tablero[i][j] == 0) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	private boolean quedanMovimientos() {
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero[i].length; j++) {
+				if (tablero[i][j] == 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    private void seleccionarModalidadJvsJ() {
-        System.out.println("1. Mostrar estadísticas J1vsJ2");
-        System.out.println("2. Reiniciar estadísticas J1vsJ2");
-        System.out.println("3. Volver a jugar");
-        System.out.println("4. Salir");
-        int opcion = entrada.nextInt();
+	private void determinarPrimerJugador() {
+		int jugadorAleatorio = random.nextInt(2) + 1;
+		jugadorActual = jugadorAleatorio;
 
-        switch (opcion) {
-            case 1:
-                mostrarEstadisticas();
-                seleccionarModalidadJvsJ();
-                break;
-            case 2:
-                reiniciarEstadisticas();
-                seleccionarModalidadJvsJ();
-                break;
-            case 3:
-                jugarPartida();
-                break;
-            case 4:
-                seleccionarModalidad();
-                break;
-            default:
-                System.out.println("Opción no válida. Por favor, seleccione nuevamente.");
-                seleccionarModalidadJvsJ();
-                break;
-        }
-    }
+		if (jugadorActual == 1) {
+			System.out.println("Empieza jugador 1 con X");
+		} else {
+			System.out.println("Empieza jugador 2 con O.");
+		}
+	}
 
-   
 }
--- src/ProyectoTresRayaPOO/UsoTresEnRaya.java
+++ src/ProyectoTresRayaPOO/UsoTresEnRaya.java
@@ -1,10 +1,85 @@
 package ProyectoTresRayaPOO;
 
+import java.util.Scanner;
+
 public class UsoTresEnRaya {
+	static Scanner in;
 
 	public static void main(String[] args) {
+		in = new Scanner(System.in);
 		TresEnRayaPOONoe juego = new TresEnRayaPOONoe();
-        juego.iniciarJuego();
+		// juego.iniciarJuego();
+		int op, op2;
+		do {
+			mostrarMenu();
+			System.out.println("Introduce una opción: ");
+			op = Integer.parseInt(in.nextLine());
+			switch (op) {
+			case 1:
+
+				juego.jugarPartida();
+				menuEstadisticas();
+				extracted(juego);
+				break;
+			case 2:
+				juego.jugarPartidaConMaquina();
+				menuEstadisticas();
+				extracted(juego);
+				break;
+			case 3:
+				juego.jugarMaquinaConMaquina();
+				break;
+			case 4:
+				System.out.println("saliendo del programa ...");
+				break;
+			default:
+				System.out.println("Opción no válida");
+				break;
+			}
+		} while (op != 4);
+	}
+
+	private static void extracted(TresEnRayaPOONoe juego) {
+		int op2;
+		System.out.println("Introduce una opcion");
+		op2 = Integer.parseInt(in.nextLine());
+		switch (op2) {
+		case 1:
+			juego.mostrarEstadisticas();
+			break;
+		case 2:
+			juego.reiniciarEstadisticas();
+			break;
+		case 3:
+			juego.jugarPartida();
+			juego.mostrarEstadisticas();
+			break;
+		case 4:
+			System.out.println("Saliendo al menu principal");
+			break;
+		default:
+			System.out.println("Opcion no válida");
+			menuEstadisticas();
+			System.out.println("Introduce una opcion");
+			op2 = Integer.parseInt(in.nextLine());
+			break;
+		}
+	}
+
+	public static void mostrarMenu() {
+		System.out.println("Bienvenido al tres en raya de Noe");
+		System.out.println("Seleccione la modalidad de juego:");
+		System.out.println("1. 2 Jugadores");
+		System.out.println("2. Jugador vs Máquina");
+		System.out.println("3. Máquina vs Máquina");
+		System.out.println("4. Salir del programa");
+	}
+
+	public static void menuEstadisticas() {
+		System.out.println("1. Mostrar estadísticas");
+		System.out.println("2. Reiniciar estadísticas");
+		System.out.println("3. Volver a jugar");
+		System.out.println("4. Salir");
 	}
 
 }
